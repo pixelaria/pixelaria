b56cd3da65eb62bd04672e255352bb410ed3ec0a
pixelaria <info@pixelaria.ru>
2018-03-22 17:22:52 +0300

    Scroll to and prices block

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -1,2 +1,2 @@
 
-$(function(){console.log('init');$('.reviews').unslider({arrows:false,dots:true,});});
+$(function(){console.log('init');$('.reviews').unslider({arrows:false,dots:true,});$('body').delegate('.scroll-to-target','click',function(e){var target=$(this).attr('href');if($(target).length){var offset=0;if($(this).data('offset')!=undefined)offset=$(this).data('offset');$.scrollTo(target,600,{offset:-offset});return false;}});});
